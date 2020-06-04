14f0e0fefe3d4c7477bd784f89fc3d2046ffe60a
Melanie3z <3100melanie@gmail.com>
2020-06-03 19:40:12 -0500

    Agregar constantes de textos que se usaran en las respuestas de la api

---
+++ config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "error_producto_no_encontrado" => "No se encontro el producto",
+    "error_de_servidor" => "Hubo un incoveniente con la solucitud, comuniquese con el administrador",
+    "error_diligenciar_campos" => "Solicitud incorrecta verifique la información, asegurese de diligenciar todos los campos",
+    "respuesta_json_ok" => "ok",
+    "respuesta_json_error" => "error",
+    "respuesta_json_mensaje" => "mensaje",
+    "respuesta_json_data" => "data",
+    "registro_exitoso" => "Se registro con exito",
+    "actualizacion_exitosa" => "Se modifico con exito",
+    "eliminacion_exitosa" => "Se elimino con exito"
+
+];
